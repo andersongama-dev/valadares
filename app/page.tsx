85bb7a2057956a3bcef9db14a21ece4c4fe11c51
andersongama-dev <andersongamasilva5@gmail.com>
2026-07-03 08:52:00 -0300

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,64 +2,151 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <div>
+      <header>
+        <p>Valadares</p>
+
+        <nav>
+          <ul>
+            <li>
+              <a href="#hero">Início</a>
+            </li>
+            <li>
+              <a href="">Soluções</a>
+            </li>
+            <li>
+              <a href="">Metodologia</a>
+            </li>
+            <li>
+              <a href="">Resultados</a>
+            </li>
+            <li>
+              <a href="">Contato</a>
+            </li>
+          </ul>
+        </nav>
+
+        <button>Solicitar Auditoria</button>
+      </header>
+
+      <section id="hero">
+        <div>
+          <p>Estratégia para operações de marketplace</p>
+          <h1>Faturamento é ego. Lucro é realidade</h1>
+          <p>
+            Escalar uma operação vai muito além de vender mais, A Valadares
+            Assessoria transforma dados em decisões estratégicas para aumnetar a
+            eficiência, preservar margem e construir operações sustentáveis
+          </p>
+
+          <article>
+            <button>Solicitar Auditoria Estratégica</button>
+            <button>Ver Metodologia</button>
+          </article>
+        </div>
+      </section>
+
+      <section>
+        <div>
+          <p>O problema</p>
+          <h2>Muitas empresas faturam bilhões, Poucas realmente lucram</h2>
+
+          <p>
+            À medida que a operação cresce, aumentam também os custos, a
+            complexidade e a dependência dos algoritmos dos marketplaces. O
+            faturamento sobe, mas a lucratividade nem sempre acompanha esse
+            crescimento. Sem uma estratégia orientada por dados, cada decisão
+            representa um risco para a margem da operação.
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <div>
+          <article>
+            <h5>Alto faturamento</h5>
+          </article>
+
+          <article>
+            <h5>Margem reduzida</h5>
+          </article>
+
+          <article>
+            <h5>Crescimento sem controle</h5>
+          </article>
+
+          <article>
+            <h5>Operação otimizada</h5>
+          </article>
         </div>
-      </main>
+      </section>
+
+      <section>
+        <div>
+          <p>O que fazemos</p>
+          <h2>Transformamos dados em vantagem competitiva</h2>
+          <p>
+            Nossa atuação combina inteligência de mercado, análise operacional e
+            otimização estratégica para tornar sua operação mais eficiente,
+            previsível e lucrativa.
+          </p>
+        </div>
+
+        <div>
+          <article>
+            <h5>Inteligência de Dados</h5>
+            <p>
+              Analisamos indicadores para identificar gargalos e oportunidades
+              de crescimento.
+            </p>
+          </article>
+          <article>
+            <h5>Estratégia Comercial</h5>
+            <p>
+              Estruturamos processos para aumentar rentabilidade sem depender
+              apenas de volume.
+            </p>
+          </article>
+          <article>
+            <h5>Otimização de Marketplace</h5>
+            <p>Melhoramos posicionamento, anúncios e eficiência operacional.</p>
+          </article>
+          <article>
+            <h5>Crescimento Sustentável</h5>
+            <p>
+              Escalamos sua operação mantendo controle financeiro e
+              previsibilidade.
+            </p>
+          </article>
+        </div>
+      </section>
+
+      <section>
+        <div>
+          <p>Nossa metodologia</p>
+          <h2>Uma metodologia baseada em dados, não em achismos</h2>
+        </div>
+
+        {/* Transofrme isso em uma timeline */}
+        <div>
+          01 Diagnóstico Estratégico Análise completa da operação. ↓ 02
+          Mapeamento de Oportunidades Identificação dos principais gargalos. ↓
+          03 Plano de Ação Estratégia personalizada. ↓ 04 Acompanhamento
+          Monitoramento contínuo dos resultados.
+        </div>
+      </section>
+
+      <section>
+        <div>
+          <p>Diferenciais</p>
+          <h2>Por que empresas escolhem a Valadares?</h2>
+        </div>
+
+        {/* Transofrme isso em um card */}
+        <div>
+          Cards 🎯 Decisões baseadas em dados 📊 Inteligência estratégica 📈
+          Foco em lucratividade ⚡ Execução personalizada 🤝 Atendimento próximo
+          🚀 Crescimento sustentável
+        </div>
+      </section>
     </div>
   );
 }
