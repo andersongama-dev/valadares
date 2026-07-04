619718a96e54db193be61b5117613c5691c878aa
andersongama-dev <andersongamasilva5@gmail.com>
2026-07-04 10:30:37 -0300

    refactor: separation into header components

--- app/page.tsx
+++ app/page.tsx
@@ -30,60 +30,13 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <div>
-      <header
-        className="
-          fixed top-8 left-1/2 -translate-x-1/2
-          w-full max-w-7xl
-          flex justify-between items-center
-          px-8 py-4
-          rounded-2xl
-          z-50
-          backdrop-blur-2xl
-          bg-blue-800
-          shadow-2xl shadow-black/10
-          "
-      >
-        <div className="flex gap-4">
-          {/* <Image alt="Logo valadares" src={""} width={32} height={32} /> */}
-          <p className="text-xl font-medium text-white">Valadares</p>
-        </div>
-        <nav>
-          <ul className="flex gap-6 text-white">
-            <li>
-              <a href="#hero" className="hover:text-blue-300 transition-colors">
-                Início
-              </a>
-            </li>
-            <li>
-              <a href="" className="hover:text-blue-300 transition-colors">
-                Soluções
-              </a>
-            </li>
-            <li>
-              <a href="" className="hover:text-blue-300 transition-colors">
-                Metodologia
-              </a>
-            </li>
-            <li>
-              <a href="" className="hover:text-blue-300 transition-colors">
-                Resultados
-              </a>
-            </li>
-            <li>
-              <a href="" className="hover:text-blue-300 transition-colors">
-                Contato
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <button className="bg-white px-4 py-3 text-base rounded-xl text-blue-800 font-medium cursor-pointer hover:bg-blue-800 hover:text-white transition-colors">
-          Solicitar Auditoria
-        </button>
-      </header>
+      <Header />
+
       <section
         id="hero"
         className="relative h-dvh flex items-center justify-center overflow-hidden"
